ace40f663430b79fe7ef5f9d6fc5819d0695b4ba
Vitaly Sukhinin <vitaliksuhinin99@gmail.com>
2024-07-22 20:02:32 -0400

    added post and delete mappings for labels. Changed return type for post method. Now return location of created label with status code 201

--- src/main/java/com/vitalysukhinin/financial_system/controllers/LabelController.java
+++ src/main/java/com/vitalysukhinin/financial_system/controllers/LabelController.java
@@ -4,8 +4,11 @@ import com.vitalysukhinin.financial_system.entities.Label;
 import com.vitalysukhinin.financial_system.repositories.LabelRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/labels")
@@ -22,8 +25,33 @@ public class LabelController {
         return ResponseEntity.ok(labelRepository.findAll());
     }
 
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Label> getLabel(@PathVariable Integer id) {
+        Optional<Label> found = labelRepository.findById(id);
+        if (found.isPresent())
+            return ResponseEntity.ok(found.get());
+         else
+            return ResponseEntity.notFound().build();
+    }
+
     @PostMapping
     public ResponseEntity<Label> createLabel(@RequestBody Label label) {
+        Label createdLabel = labelRepository.save(label);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(createdLabel.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> deleteLabel(@PathVariable Integer id) {
+        labelRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<Label> updateLabel(@RequestBody Label label) {
         return ResponseEntity.ok(labelRepository.save(label));
     }
 }
